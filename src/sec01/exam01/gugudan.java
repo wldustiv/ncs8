1824c9c1a28cb7d73749987d3d343cb4dc2dfc57
Baek jiyeon <wldustivx@naver.com>
2022-09-07 18:58:48 +0900

    first commit

---
+++ src/sec01/exam01/gugudan.java
@@ -0,0 +1,16 @@
+package sec01.exam01;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		for(int i=2; i<10; i++){
+			System.out.println(i+"단");
+			
+			for(int j=1; j<10; j++){
+				System.out.println(i + "x" + j + "=" +  i*j);
+			}
+			System.out.println();
+		}
+	}
+
+}
